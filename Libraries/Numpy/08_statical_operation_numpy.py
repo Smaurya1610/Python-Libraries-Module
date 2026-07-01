3d758fb506381d65a89f616e245a5e544ab2c05a
Sameer Kushwaha <sameerkushwaha1610@gmail.com>
2026-07-01 21:21:56 +0530

    Statical operation

---
+++ Libraries/Numpy/08_statical_operation_numpy.py
@@ -0,0 +1,35 @@
+# ----------------------
+# Statistical operation in 1D
+# ----------------------
+'''
+max()
+min()
+sum()
+mean()
+median()
+prod()
+var()
+std()
+average()
+'''
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Array:", arr)
+
+print("max()     =", np.max(arr))
+print("min()     =", np.min(arr))
+print("sum()     =", np.sum(arr))
+print("mean()    =", np.mean(arr))
+print("median()  =", np.median(arr))
+print("prod()    =", np.prod(arr))
+print("var()     =", np.var(arr))
+print("std()     =", np.std(arr))
+print("average() =", np.average(arr))
+
+
+
+'''var()	Data kitna spread hai	mean se 
+std()	Spread ka square root [var k square root]'''
+
